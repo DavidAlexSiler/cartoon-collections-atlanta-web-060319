99861ad2092789bccf8eaad4f5190276cfad6fbf
Alex Siler <davidalexsiler@gmail.com>
2019-05-27 19:02:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,8 @@ def long_planeteer_calls(calls)
   new_arr.include?(5)
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)
+  ingredients.include?(cheese_types)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
